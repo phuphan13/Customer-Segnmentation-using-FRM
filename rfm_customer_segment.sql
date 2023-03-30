8f1369f948c38447ae2dfe027ee29b151caddf42
Phu Phan <89576717+phuphan13@users.noreply.github.com>
2023-03-31 08:32:26 +0930

    Add files via upload

--- rfm_customer_segment.sql
+++ rfm_customer_segment.sql
@@ -6,7 +6,7 @@ drop table if exists rfm_customer_analysis;
 drop table if exists rfm_country_analysis;
 
 /* 
-    CREATING SEGMENT TABLE BASED ON THE SCORES OF RECENCY, FREQUENCY AND MONETARY
+    CREATING SEGMENTATION TABLE BASED ON THE SCORES OF RECENCY, FREQUENCY AND MONETARY
     FOR DEMONSTRATION, EACH SCORE WILL RANG FROM VALUE 1 TO 3 REPRESENTING FROM WORSE TO BEST 
 */
 
@@ -34,9 +34,12 @@ insert into segmentation values ('Lost Customers','Lowest recency, frequency & m
         Monetary: how much has this customer spent in total 
 
 */
+
+/* Valuation date can be changed up your business */
 declare @valuation_date as DATE
 set @valuation_date = '1998-05-10';
 
+/* Getting customer with lastest purchase date compared to valuation date, how many orders made and totla sales */
 with rfm_customer (custid, recency, frequency, monetary) 
 as                (
                        select c.customerid as custid,
@@ -48,7 +51,8 @@ as                (
                        join [order details] o2 on o1.orderid = o2.orderid 
                        group by c.customerid
                    ),
-/* Creating rfm score table using ntile (could be quartile, quintile or percentile) metric */
+
+/* Creating rfm score table using ntile method (or it can be quartile, quintile or percentile) */
 rfm_scores (custid, recency, frequency, monetary, r_score, f_score, m_score) 
 as (  
     select custid, recency, frequency, monetary,
@@ -64,7 +68,7 @@ as (
     select *, CONCAT(r_score,f_score,m_score) as rfm_score from rfm_scores)
 
 
-/* Main query*/
+/* Main query */
 select t1.*, ISNULL(t2.segment,'Others') as 'segment'
 into rfm_customer_analysis
 from rfm_final as t1 left join segmentation as t2 
@@ -87,7 +91,8 @@ as                (
                        join [order details] o2 on o1.orderid = o2.orderid 
                        group by c.country
                    ),
-/* Creating rfm score table using ntile (could be quartile, quintile or percentile) metric */
+
+/* Creating rfm score table using ntile method (or it can be quartile, quintile or percentile) */
 rfm_scores (country, recency, frequency, monetary, r_score, f_score, m_score) 
 as (  
     select country, recency, frequency, monetary,
@@ -102,7 +107,7 @@ rfm_final
 as (
     select *, CONCAT(r_score,f_score,m_score) as rfm_score from rfm_scores)
 
-/* Main query*/
+/* Main query */
 select t1.*, ISNULL(t2.segment,'Others') as 'segment'
 into rfm_country_analysis
 from rfm_final as t1 left join segmentation as t2
@@ -110,5 +115,6 @@ on (t1.r_score between t2.recency_f and t2.recency_t) AND
    (t1.f_score between t2.frequency_f and t2.frequency_t) AND
    (t1.m_score between t2.monetary_f and t2.monetary_t);
 
+select * from rfm_customer_analysis;
 select * from rfm_country_analysis;
 
